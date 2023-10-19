f6b03fce02e5829ec4682c65d654704ed7dcb287
anacarol21112006 <106815862+anacarol21112006@users.noreply.github.com>
2023-10-19 11:26:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,25 @@ function tocaSom (selectorAudio) {
 
     
 }
+ const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+ //para
+ for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+
+    const tecla = ListaDeTeclas[contador];
+    const intrumento = tecla.classList[1];
+    const idAudio = '#som_${instrumento}';// template string
+
+    tecla.oncliclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space || evneto.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () { tecla.classList.remove('ativa')
+}
